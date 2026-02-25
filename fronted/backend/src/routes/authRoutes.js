bfff4ec8c3312f7f9cf42722522a9ee820f47639
nida-affifah <nidaduwaa@gmail.com>
2026-02-25 16:36:10 +0700

    remove .env from repository

--- fronted/backend/src/routes/authRoutes.js
+++ fronted/backend/src/routes/authRoutes.js
@@ -1,9 +1,17 @@
 import express from "express";
 import { login, register } from "../controllers/authController.js";
+import { authMiddleware } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/login", login);
 router.post("/register", register);
+
+router.get("/me", authMiddleware, (req, res) => {
+  res.json({
+    id: req.user.id,
+    username: req.user.username,
+  });
+});
 
 export default router;
